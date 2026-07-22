df716c843ab2288a1703aa8b3c7cc1bfb151c2a8
RobTech <167295276+BesongFred@users.noreply.github.com>
2026-07-22 20:29:12 +0100

    Allow external image domains for gallery (unsplash, pexels)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'images.pexels.com',
+      'images.unsplash.com'
+    ],
+  },
 }
 module.exports = nextConfig
